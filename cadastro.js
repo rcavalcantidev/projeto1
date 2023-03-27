0156aca66f69dc1130829d2362eedc23bd6c9a25
rcavalcantidev <rcavalcantidev@gmail.com>
2023-03-27 19:46:48 -0300

    adicionando funcionalidade

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,15 @@
 function signup (email)
 return email;
-}
+
 
 //pegando o email e cadastrando na plataforma.
+
+
+function pega email (email)
+return 'bem vindo ao sistema'
+
+//adicionando funcao de mensagem
+
+
+
+
